86f11711dd327610c6745aa44b7db4c4075c23d8
Jason White <jmw5598@gmail.com>
2023-03-18 05:55:39 -0400

    Initial commit

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-monorepo-angular-nestjs-microservices-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'admin';
+}
